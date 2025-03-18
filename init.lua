af3c3029640a005e75b0d4880130bf42d0685001
MateuszKnopp <knoppmateusz@gmail.com>
2025-03-18 14:59:29 +0100

    Golang neotest - Neovim config - version 0.1

--- init.lua
+++ init.lua
@@ -728,6 +728,30 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-go',
+    },
+    opts = {},
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-go',
+        },
+        output_panel = {
+          enabled = true,
+          open = 'botright split | resize 15',
+        },
+        quickfix = {
+          open = false,
+        },
+      }
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
